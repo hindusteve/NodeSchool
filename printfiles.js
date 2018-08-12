0fdad2a797771476914784b14572c2bde108552b
Abhijit Khanna <hindu_steve@yahoo.com>
2018-08-12 15:41:48 -0400

    updated program file and module for learnyounode challenge 6. Did careful study to understand call sequences and program flow. First attempt, will test and change as necessary.

---
+++ printfiles.js
@@ -0,0 +1,23 @@
+var path = require('path')
+
+module.exports = function (folder, fileExtension, callback) {
+
+
+	// 08/12 - need to create a variable to hold files I want
+	// 08/12 - sticking a little on idea of cb that only calls err, or calls (null, files)
+	fs.readdir(folder, function cb(err, files) { // cb is a call back, files is a variable that will be the array output of readdir
+		var filteredFiles = new Array;
+
+		if(err)
+			return(callback(err));
+
+		files.foreach(function(file){
+			if(('.' + path.extname(file)) === fileExtension) {
+				// code to add current 'file' to filteredFiles
+				filteredFiles.push(file);
+			}
+		})
+		return(callback(null, filteredFiles));
+	})
+}
+
