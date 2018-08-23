21f557ef922ebac99c0120550d92a71ababc77b4
Abhijit Khanna <hindu_steve@yahoo.com>
2018-08-23 12:13:11 -0800

    first attempt at lesson 8, using bl package to collect all data, not just the first data event, and output the # of characters, then the complete string of characters. Commit includes install of bl package

--- program.js
+++ program.js
@@ -180,38 +180,8 @@ myModule(directoryName, ext, fileList);
 
  ─────────────────────────────────────────────────────────────────────────────
 
- ## HINTS
-
-  For this exercise you will need to use the http core module.
-
-  Documentation on the http module can be found by pointing your browser
-  here:
-  file://C:\Users\abhij\AppData\Roaming\npm\node_modules\learnyounode\node_a
-  pidoc\http.html
-
-  The http.get() method is a shortcut for simple GET requests, use it to
-  simplify your solution. The first argument to http.get() can be the URL
-  you want to GET; provide a callback as the second argument.
-
-  Unlike other callback functions, this one has the signature:
-
-     function callback (response) { |* ... *| }
 
 
-  Where the response object is a Node Stream object. You can treat Node
-  Streams as objects that emit events. The three events that are of most
-  interest are: "data", "error" and "end". You listen to an event like so:
-
-     response.on("data", function (data) { |* ... *| })
-
-  The "data" event is emitted when a chunk of data is available and can be
-  processed. The size of the chunk depends upon the underlying data source.
-
-  The response object / Stream that you get from http.get() also has a
-  setEncoding() method. If you call this method with "utf8", the "data"
-  events will emit Strings rather than the standard Node Buffer objects
-  which you have to explicitly convert to Strings.
-  */
 
 var http = require('http');
 
@@ -222,14 +192,37 @@ http.get(targetUrl, function(response) {
 		console.log(data);
 	}).setEncoding("utf8")
 	});
+	
+  */
 
-/*  http.get("http://www.google.com/index.html", function(res) {
-  		console.log("Got response: " + res.statusCode);
-	}).on('error', function(e) {
-  		console.log("Got error: " + e.message);
-	});
+/*
 
-  function cb(response) {
-  	response.on("data", function(data){
+ ## HTTP COLLECT (Exercise 8 of 13)
 
-  	});*/
+  Write a program that performs an HTTP GET request to a URL provided to you
+  as the first command-line argument. Collect all data from the server (not
+  just the first "data" event) and then write two lines to the console
+  (stdout).
+
+  The first line you write should just be an integer representing the number
+  of characters received from the server. The second line should contain the
+  complete String of characters sent by the server.
+
+*/
+
+var http = require('http');
+var bl = require('bl');
+
+var targetUrl = process.argv[2];
+
+/*http.get(targetUrl, function(response) {
+	response.on('data', function(data) {
+		console.log(data);
+	}).setEncoding("utf8")
+	});*/
+
+http.get(targetUrl).pipe(bl(function(err, data) {
+	console.log(bl.length);
+	console.log(data.toString())
+}));
+
